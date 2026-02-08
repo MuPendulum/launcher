9c88b1ee6371f8cb4b7539a16c1b4957a4716731
Shimizu Hikaru <MuPendulum@users.noreply.github.com>
2026-02-09 00:03:34 +0900

    linux x86-64: use prebuilt binaries from upstream
    We are only interested in updating the bundled JRE and
    changing the packr config. We do not need nor want to rebuild binaries.
    
    This is a nasty hack so it doesn't need to be pretty

--- build-linux-x86_64.sh
+++ build-linux-x86_64.sh
@@ -2,13 +2,8 @@
 
 set -e
 
-echo Launcher sha256sum
-sha256sum build/libs/RuneLite.jar
-
-pushd native
-cmake -B build-x64 .
-cmake --build build-x64 --config Release
-popd
+RUNELITE_LAUNCHER_VERSION="2.7.7"
+RUNELITE_LAUNCHER_CHKSUM="85a0d34abef6c08561018c3600c94db113125d69318fe71fc1fe4a7ea2d49c6c"
 
 umask 022
 
@@ -17,21 +12,34 @@ source .jdk-versions.sh
 rm -rf build/linux-x64
 mkdir -p build/linux-x64
 
+mkdir -p bin
+
+if ! [ -f bin/RuneLite.AppImage ] ; then
+    curl -Lo bin/RuneLite.AppImage \
+        https://github.com/runelite/launcher/releases/download/${RUNELITE_LAUNCHER_VERSION}/RuneLite.AppImage
+    chmod +x bin/RuneLite.AppImage
+fi
+
+echo "$RUNELITE_LAUNCHER_CHKSUM bin/RuneLite.AppImage" | sha256sum -c
+
 if ! [ -f linux64_jre.tar.gz ] ; then
     curl -Lo linux64_jre.tar.gz $LINUX_AMD64_LINK
 fi
 
 echo "$LINUX_AMD64_CHKSUM linux64_jre.tar.gz" | sha256sum -c
 
+pushd bin
+./RuneLite.AppImage --appimage-extract
+popd
+
 # Note: Host umask may have checked out this directory with g/o permissions blank
 chmod -R u=rwX,go=rX appimage
-# ...ditto for the build process
-chmod 644 build/libs/RuneLite.jar
 
-cp native/build-x64/src/RuneLite build/linux-x64/
-cp build/libs/RuneLite.jar build/linux-x64/
+mkdir -p build/linux-x64/
+cp bin/squashfs-root/RuneLite build/linux-x64/
+cp bin/squashfs-root/RuneLite.jar build/linux-x64/
 cp packr/linux-x64-config.json build/linux-x64/config.json
-cp build/filtered-resources/runelite.desktop build/linux-x64/
+cp bin/squashfs-root/runelite.desktop build/linux-x64/
 cp appimage/runelite.png build/linux-x64/
 mkdir -p build/linux-x64/usr/share/icons/hicolor/128x128/apps/
 cp appimage/runelite.png build/linux-x64/usr/share/icons/hicolor/128x128/apps/
