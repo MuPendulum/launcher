69ba54861ec2ddabe2097841f9cb7458140a8c80
Shimizu Hikaru <MuPendulum@users.noreply.github.com>
2026-02-09 00:02:36 +0900

    linux x86-64: update bundled JRE to 17 LTS

--- .jdk-versions.sh
+++ .jdk-versions.sh
@@ -19,9 +19,9 @@ MAC_AARCH64_RELEASE=jdk-17.0.18+8
 MAC_AARCH64_CHKSUM=6853987fa37340b157d7e8e895db0148efa13c3b2d6e6f3b289aac42e437d32e
 MAC_AARCH64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.18%2B8/OpenJDK17U-jre_aarch64_mac_hotspot_17.0.18_8.tar.gz
 # linux x64
-LINUX_AMD64_RELEASE=jdk-11.0.30+7
-LINUX_AMD64_CHKSUM=d851e43d81ec6ff7f28efe28c42b4787a045e8f59cdcd6434dece98d8342eb8a
-LINUX_AMD64_LINK=https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.30%2B7/OpenJDK11U-jre_x64_linux_hotspot_11.0.30_7.tar.gz
+LINUX_AMD64_RELEASE=jdk-17.0.18+8
+LINUX_AMD64_CHKSUM=8b418e38cca87945858ae911988401720095eb671357d47437b4adb49c28dcab
+LINUX_AMD64_LINK=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.18%2B8/OpenJDK17U-jre_x64_linux_hotspot_17.0.18_8.tar.gz
 # linux aarch64
 LINUX_AARCH64_RELEASE=jdk-11.0.30+7
 LINUX_AARCH64_CHKSUM=9d6a8d3a33c308bbc7332e4c2e2f9a94fbbc56417863496061ef6defef9c5391
